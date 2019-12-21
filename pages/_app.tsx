63b32caa586e270b3c18098b6f5c37d306cac02a
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2019-12-21 17:47:37 +0700

    Initial commit, add Nextjs and login page

---
+++ pages/_app.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import App from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../src/theme';
+
+export default class MintApp extends App {
+    componentDidMount() {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles && jssStyles.parentElement) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }
+
+    render() {
+        const { Component, pageProps } = this.props;
+
+        return (
+            <React.Fragment>
+                <Head>
+                    <title>Mint by HMIF Tech</title>
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+                    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,700,900&display=swap" rel="stylesheet" />
+
+                    <style jsx global>{`
+                      .font-black {
+                        font-weight: 900 !important;
+                      }
+                    `}</style>
+                </Head>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Component {...pageProps} />
+                </ThemeProvider>
+            </React.Fragment>
+        );
+    }
+}
---
+++ pages/index.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import Container from "@material-ui/core/Container/Container";
+import Typography from "@material-ui/core/Typography/Typography";
+import Box from "@material-ui/core/Box/Box";
+import TextField from "@material-ui/core/TextField/TextField";
+import Select from "@material-ui/core/Select/Select";
+import MenuItem from "@material-ui/core/MenuItem/MenuItem";
+import Button from "@material-ui/core/Button/Button";
+
+type MyState = {
+    count: number; // like this
+};
+
+export default class App extends React.Component<MyState> {
+    state: MyState = {
+        // optional second annotation for better type inference
+        count: 5
+    };
+    render() {
+        return (
+            <div>
+                <Container maxWidth="xs">
+                    <Box display="flex" flexDirection="column" justifyContent="center" style={{minHeight: '100vh'}} pt={3} pb={3}>
+                        <Box>
+                            <Typography variant="h3" component="span" color="primary" className="font-black">
+                                Mint
+                            </Typography>
+                            &nbsp; &nbsp;
+                            <Typography variant="h5" component="span" color="primary">
+                                by HMIF Tech
+                            </Typography>
+                        </Box>
+                        <Box>
+                            <p>
+                                Sebuah alat bantu untuk melaksanakan <i>mock interview</i> dengan teman-teman.
+                            </p>
+                        </Box>
+                        <Box mt={3}>
+                            <b>Apa NIM kamu?</b>
+                            <TextField variant="outlined" margin="dense" fullWidth />
+                        </Box>
+                        <Box mt={3}>
+                            <b>Apa NIM teman kamu yang diwawancara?</b>
+                            <TextField variant="outlined" margin="dense" fullWidth />
+                        </Box>
+                        <Box mt={3}>
+                            <b>Tipe interview</b>
+                            <Box mt={1}>
+                                <Select variant="outlined" margin="dense" fullWidth>
+                                    <MenuItem value="engineeringRoles">
+                                        Technical/Software Engineering
+                                    </MenuItem>
+                                    <MenuItem value="productRoles">
+                                        Product-related roles
+                                    </MenuItem>
+                                </Select>
+                            </Box>
+                        </Box>
+                        <Box mt={3}>
+                            <Button variant="contained" color="primary" style={{width: '100%', textTransform: 'none'}}>
+                                Lanjut
+                            </Button>
+                        </Box>
+                    </Box>
+                </Container>
+            </div>
+        );
+    }
+}
